5e5d8d2c312cb8ee4556242e6a2f99ae86d3eb21
emilylakic <emilylakic@gmail.com>
2019-12-07 23:44:54 -0500

    Added ability to navigate to second screen

--- app/src/main/java/com/example/myapplication/finalproject/MainActivity.java
+++ app/src/main/java/com/example/myapplication/finalproject/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.myapplication.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextButton = (Button) findViewById(R.id.button);
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondScreen.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
