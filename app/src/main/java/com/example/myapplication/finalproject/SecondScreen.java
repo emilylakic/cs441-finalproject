c572e80f7709b162c0229627931b2e276f5d7bec
emilylakic <emilylakic@gmail.com>
2019-12-11 15:50:04 -0500

    Added video capability to second screen

--- app/src/main/java/com/example/myapplication/finalproject/SecondScreen.java
+++ app/src/main/java/com/example/myapplication/finalproject/SecondScreen.java
@@ -1,8 +1,10 @@
 package com.example.myapplication.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class SecondScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SecondScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.snow;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
