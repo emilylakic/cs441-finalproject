d8fa1fc2f02f32bd477ba895717abdf55d52cd3e
emilylakic <emilylakic@gmail.com>
2019-12-14 15:17:23 -0500

    Finished second and third screen, added powerful female roles

--- app/src/main/java/com/example/myapplication/finalproject/SecondScreen.java
+++ app/src/main/java/com/example/myapplication/finalproject/SecondScreen.java
@@ -1,17 +1,26 @@
 package com.example.myapplication.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class SecondScreen extends AppCompatActivity {
 
+    Button goto3;
+    Button goto1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+        goto3 = (Button) findViewById(R.id.goto3);
+        goto1 = (Button) findViewById(R.id.goto1);
         VideoView videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.googleflights;
         Uri uri = Uri.parse(videoPath);
@@ -20,5 +29,25 @@ public class SecondScreen extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+        goto3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondScreen.this, ThirdScreen.class);
+
+                startActivity(intent);
+            }
+        });
+
+        goto1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondScreen.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
